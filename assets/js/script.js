19de9b157f06034548f838f49ac3694266934971
ryujinmax <santi.rra40@gmail.com>
2022-11-18 21:00:17 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,14 +28,14 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Influencer", "Video Editor"],
+        strings: ["Developer", "Influencer", "Designer", "Video Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Influencer", "Video Editor"],
+        strings: ["Developer", "Influencer", "Designer", "Video Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
